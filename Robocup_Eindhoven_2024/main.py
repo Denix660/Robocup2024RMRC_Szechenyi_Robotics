df1a15273e264f723e4c2975c033d182512e7345
Denix660 <126967005+Denix660@users.noreply.github.com>
2024-07-16 12:16:32 +0200

    updated arm
    The arm value is visible in live action

--- Robocup_Eindhoven_2024/main.py
+++ Robocup_Eindhoven_2024/main.py
@@ -11,88 +11,125 @@ speed = 0.9
 motor1 = Motor(23,27)
 motor1_enable = OutputDevice(5, initial_value=1)
 motor2 = Motor(25,24)
-motor2_enable = OutputDevice(17, initial_value=1)
+motor2_enable = OutputDevice(12, initial_value=1)
 
 
 pwm = Adafruit_PCA9685.PCA9685(busnum = 1)
 # pwm = Adafruit_PCA9685.PCA9685()
-pwm.set_pwm_freq(70)
+pwm.set_pwm_freq(50)
 
-servo_min = 150  # Minimum servo pulse length
-servo_max = 600  # Maximum servo pulse length
+servo_min = 205  # Minimum servo pulse length
+servo_max = 496  # Maximum servo pulse length
 
 servo_angle = 5
+fogokar_angle = 15
 
-
-
-servo_1 = 14
+servo_1 = 12
 servo_2 = 13
-servo_3 = 15
-servo_4 = 12
+servo_3 = 14
+servo_4 = 15
 
 
-servo1_degree = 0
-servo2_degree = 0
-servo3_degree = 0
+servo1_degree = 45
+servo2_degree = 45
+servo3_degree = 90
 servo4_degree = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
+                if event.key == pygame.K_UP:
+                    if speed < 0.9:
+                        speed += 0.1
+                        print(speed," is the current speed")
+                elif event.key == pygame.K_DOWN:
+                    if speed > 0.1:
+                        speed -= 0.1
+                        print(speed, "is the current speed")
+                elif event.key == pygame.K_a:
                     print("a")
                     motor1.backward(speed)
                     motor2.forward(speed)
-                if event.key == pygame.K_d:
+                elif event.key == pygame.K_d:
                     print("d")
                     motor1.forward(speed)
                     motor2.backward(speed)
-                if event.key == pygame.K_w:
+                elif event.key == pygame.K_w:
                     print("w")
                     motor1.forward(speed)
                     motor2.forward(speed)
-                if event.key == pygame.K_s:
+                elif event.key == pygame.K_s:
                     print("s")
                     motor1.backward(speed)
                     motor2.backward(speed)
             #mozgás
 
-                if event.key == pygame.K_r:
-                    servo1_degree += servo_angle
-                    servo_1_pwm = int((servo1_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_1, 0, servo_1_pwm)
-                if event.key == pygame.K_f:
-                    servo1_degree -= servo_angle
-                    servo_1_pwm = int((servo1_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_1, 0, servo_1_pwm)
-
-                if event.key == pygame.K_t:
-                    servo2_degree += servo_angle
-                    servo_2_pwm = int((servo2_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_2,0,servo_2_pwm)
-                if event.key == pygame.K_g:
-                    servo2_degree -= servo_angle
-                    servo_2_pwm = int((servo2_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_2,0,servo_2_pwm)
-
-                if event.key == pygame.K_z:
-                    servo3_degree += servo_angle
-                    servo_3_pwm = int((servo3_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_3, 0, servo_3_pwm)
-                if event.key == pygame.K_h:
-                    servo3_degree -= servo_angle
-                    servo_3_pwm = int((servo3_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_3, 0, servo_3_pwm)
+                elif event.key == pygame.K_r:
+                    if servo1_degree < 180:
+                        servo1_degree += servo_angle
+                        servo_1_pwm = int((servo1_degree / 180.0) * (servo_max - servo_min) + servo_min)
+                        print("szervo1 pwm:", servo_1_pwm)
+                        pwm.set_pwm(servo_1, 0, servo_1_pwm)
+                        print("1 szervó foka:", servo1_degree)
 
 
-                if event.key == pygame.K_u:
-                    servo4_degree += 15
-                    servo_4_pwm = int((servo4_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_4, 0, servo_4_pwm)
-                if event.key == pygame.K_j:
-                    servo4_degree -= 15
-                    servo_4_pwm = int((servo4_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_4, 0, servo_4_pwm)
+                elif event.key == pygame.K_f:
+                    if servo1_degree > 0 :
+                        servo1_degree -= servo_angle
+                        servo_1_pwm = int((servo1_degree / 180.0) * (servo_max - servo_min) + servo_min)
+                        print("szervo1 pwm:", servo_1_pwm)
+                        pwm.set_pwm(servo_1, 0, servo_1_pwm)
+                        print("1 szervó foka:", servo1_degree)
+
+
+                elif event.key == pygame.K_t:
+                    if servo2_degree < 180:
+                        servo2_degree += servo_angle
+                        servo_2_pwm = int((servo2_degree / 180.0) * (servo_max - servo_min) + servo_min)
+                        print("szervo2 pwm:", servo_2_pwm)
+                        pwm.set_pwm(servo_2,0,servo_2_pwm)
+                        print("2 szervó foka:", servo2_degree)
+
+
+                elif event.key == pygame.K_g:
+                    if servo2_degree > 0:
+                        servo2_degree -= servo_angle
+                        servo_2_pwm = int((servo2_degree / 180.0) * (servo_max - servo_min) + servo_min)
+                        print("szervo2 pwm:", servo_2_pwm)
+                        pwm.set_pwm(servo_2,0,servo_2_pwm)
+                        print("2 szervó foka:", servo2_degree)
+
+                elif event.key == pygame.K_z:
+                    if servo3_degree < 180:
+                        servo3_degree += servo_angle
+                        servo_3_pwm = int((servo3_degree / 180.0) * (servo_max - servo_min) + servo_min)
+                        print("szervo3 pwm:", servo_3_pwm)
+                        pwm.set_pwm(servo_3, 0, servo_3_pwm)
+                        print("3 szervó foka:", servo3_degree)
+
+                elif event.key == pygame.K_h:
+                    if servo3_degree > 0:
+                        servo3_degree -= servo_angle
+                        servo_3_pwm = int((servo3_degree / 180.0) * (servo_max - servo_min) + servo_min)
+                        print("szervo3 pwm:", servo_3_pwm)
+                        pwm.set_pwm(servo_3, 0, servo_3_pwm)
+                        print("3 szervó foka:", servo3_degree)
+
+                elif event.key == pygame.K_u:
+                    if servo4_degree < 180:
+                        servo4_degree += fogokar_angle
+                        servo_4_pwm = int((servo4_degree / 180.0) * (servo_max - servo_min) + servo_min)
+                        print("szervo4 pwm:", servo_4_pwm)
+                        pwm.set_pwm(servo_4, 0, servo_4_pwm)
+                        print("4 szervó foka:", servo4_degree)
+                elif event.key == pygame.K_j:
+                    if servo4_degree > 0:
+                        servo4_degree -= fogokar_angle
+                        servo_4_pwm = int((servo4_degree / 180.0) * (servo_max - servo_min) + servo_min)
+                        print("szervo4 pwm:", servo_4_pwm)
+                        pwm.set_pwm(servo_4, 0, servo_4_pwm)
+                        print("4 szervó foka:", servo4_degree)
+
             #kar
                 if event.key == pygame.K_p:
                         pygame.quit()
@@ -110,42 +147,3 @@ while True:
                 if event.key == pygame.K_s:
                         motor1.stop()
                         motor2.stop()
-
-                if event.key == pygame.K_r:
-                    servo1_degree += 0
-                    servo_1_pwm = int((servo1_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_1,0,servo_1_pwm)
-                if event.key == pygame.K_f:
-                    servo1_degree -= 0
-                    servo_1_pwm = int((servo1_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_1,0,servo_1_pwm)
-
-
-                if event.key == pygame.K_t:
-                    servo2_degree += 0
-                    servo_2_pwm =int((servo2_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_2,0,servo_2_pwm)
-                if event.key == pygame.K_g:
-                    servo2_degree -= 0
-                    servo_2_pwm =int((servo2_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_2,0,servo_2_pwm)
-
-
-                if event.key == pygame.K_z:
-                    servo3_degree += 0
-                    servo_3_pwm = int((servo3_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_3,0,servo_3_pwm)
-                if event.key == pygame.K_h:
-                    servo3_degree -= 0
-                    servo_3_pwm = int((servo3_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_3,0,servo_3_pwm)
-
-
-                if event.key == pygame.K_u:
-                    servo4_degree += 0
-                    servo_4_pwm = int((servo4_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_4,0,servo_4_pwm)
-                if event.key == pygame.K_j:
-                    servo4_degree -= 0
-                    servo_4_pwm = int((servo4_degree / 180.0) * (servo_max - servo_min) + servo_min)
-                    pwm.set_pwm(servo_4,0,servo_4_pwm)
